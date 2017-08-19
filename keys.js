6a2f342e1d882cc645b9877ae65481abb40d205b
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-08-19 18:46:42 -0400

    first commit with initial information

---
+++ keys.js
@@ -0,0 +1,11 @@
+exports.twitterKeys = {
+  consumer_key: 'qyb95rUdcxHJszFqDLasG0WXH',
+  consumer_secret: 'hEvnWusTdIud3v85HrkhTkOI8c5P5fx5bRGoUg4QcafQCCqEm8',
+  access_token_key: '896434993660133380-3mDI9cTcoLPyohmVJKeo9AGVKQ4y6cN',
+  access_token_secret: 'CVarQswbC9UB49RKPIMXTSR79U6MUxFySvy638pyp2YDK',
+};
+
+exports.spotifyKeys = {
+  id: '76d1860f380640679052083a865afcd0',
+  secret: '76e5cb800fb440debba16f70e1164035'
+};
