7c7e9ca17adc82a639d07b34b6a931d9e5ffd410
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-08-20 13:07:45 -0400

    spotify api works using inquierer commands

--- liri.js
+++ liri.js
@@ -1,19 +1,54 @@
 var twitterPackage = require('twitter');
-var spotifyPackage = require('node-spotify-api');
 var keysFile = require('./keys.js');
-var twitterApiKey = keysFile.keys;
-var spotifyApiKey = keysFile.spotifyKey;
-var fs = require("fs")
-
-fs.readFile("random.txt", "utf8", function(error, data) {
-
-  if (error) {
-    return console.log(error);
-  }
-
-  var dataArr = data.split(",");
-  // console.log(data);
+var spotifyPackage = require('node-spotify-api');
+var spotify = new spotifyPackage({
+  id: '76d1860f380640679052083a865afcd0',
+  secret: '76e5cb800fb440debba16f70e1164035'
 });
+var fs = require("fs");
+var inquirer = require("inquirer");
+//-------------------------------------
+inquirer.prompt([
+    {
+      name: 'song',
+      message: 'What song would you like to hear?'
+
+    }
+  ]).then(function(answers){
+spotify.search({ type: 'track', query: answers.song, limit: 1 }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+  var albumInfo = data.tracks.items[0].album; 
+  console.log(albumInfo);
+  });
+//-------------------------------------
+  //spotify schtuff
+
+ 
+
+// console.log(albumInfo);
+});
+//-------------------------------------
 
 
-console.log(twitterApiKey);
+
+
+
+
+
+
+
+// //read random.txt file
+// fs.readFile("random.txt", "utf8", function(error, data) {
+
+//   if (error) {
+//     return console.log(error);
+//   }
+
+//   var dataArr = data.split(",");
+//   // console.log(data);
+// });
+
+
+
