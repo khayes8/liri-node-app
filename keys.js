8ffa7aea9d912873f85ad1d2efe50afcd777eeed
Kelsey Hayes <kelseyhayes@Kelseys-MBP.home>
2017-08-20 10:52:37 -0400

    liri.js connected to twiiter and spotify apis and node packages

--- keys.js
+++ keys.js
@@ -9,3 +9,17 @@ exports.spotifyKeys = {
   id: '76d1860f380640679052083a865afcd0',
   secret: '76e5cb800fb440debba16f70e1164035'
 };
+
+// exports.keys = {
+// twitterKey: {
+//   consumer_key: 'qyb95rUdcxHJszFqDLasG0WXH',
+//   consumer_secret: 'hEvnWusTdIud3v85HrkhTkOI8c5P5fx5bRGoUg4QcafQCCqEm8',
+//   access_token_key: '896434993660133380-3mDI9cTcoLPyohmVJKeo9AGVKQ4y6cN',
+//   access_token_secret: 'CVarQswbC9UB49RKPIMXTSR79U6MUxFySvy638pyp2YDK',
+// }
+// spotifyKey: {
+//   id: '76d1860f380640679052083a865afcd0',
+//   secret: '76e5cb800fb440debba16f70e1164035'
+// }
+// };
+
--- liri.js
+++ liri.js
@@ -1,36 +1,15 @@
 var twitter = require('twitter');
-var twitterApiKey = require('./keys.js');
-// console.log(twitterApiKey);
-
-// var spotify = require('node-spotify-api');
-// var spotifyApiKey = require('./keys.js')
-// console.log(spotifyApiKey);
- 
+var spotify = require('node-spotify-api');
+var keys = require('./keys.js');
 var fs = require("fs")
 
 fs.readFile("random.txt", "utf8", function(error, data) {
-  // If the code experiences any errors it will log the error to the console.
+
   if (error) {
     return console.log(error);
   }
-  // We will then print the contents of data
-  // console.log(data);
-  // Then split it by commas (to make it more readable)
+
   var dataArr = data.split(",");
-  // We will then re-display the content as an array for later use.
-  console.log(dataArr[1]);
 });
 
 
-// var spotify = new Spotify({
-//   id: '76d1860f380640679052083a865afcd0',
-//   secret: '76e5cb800fb440debba16f70e1164035'
-// });
- 
-// spotify.search({ type: 'track', query: searchValue }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data); 
-// });
